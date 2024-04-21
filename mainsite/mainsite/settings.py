5ee3733378e3e3f5147538129e5f790c060f6899
Aleksei Pashkov <30432858+Shomnt@users.noreply.github.com>
2024-04-21 12:59:56 +0300

    Connect Database

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'mainsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'OnlineLibrary',
+        'USER': 'django_admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
